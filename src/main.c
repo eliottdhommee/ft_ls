27cfbf36ee1f49dc0a0cdab0110ce49fd2746753
eliott dhommee <eliottdhommee@MacBook-Air-de-eliott.local>
2017-07-19 14:45:52 +0200

    removed lib

--- src/main.c
+++ src/main.c
@@ -6,8 +6,24 @@
 /*   By: edhommee <eliottdhommee@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/26 11:45:40 by edhommee          #+#    #+#             */
-/*   Updated: 2017/06/26 11:45:55 by edhommee         ###   ########.fr       */
+/*   Updated: 2017/07/19 12:06:27 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <ft_ls.h>
+
+int			main(void)
+{
+	t_btree *root;
+	char		*i;
+	char		*j;
+	char		*k;
+	char		*l;
+
+	i = "afd";
+	j = "gerqf";
+	k = "refgregk";
+	l = "pjoigjerhh";
+	btree_insert_data(&root, &i, ft_strcmp);
+	return (0);
+}
